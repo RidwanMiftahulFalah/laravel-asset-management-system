575e077d69d961b75e42fd1f8a03e0ffb87e8215
RidwanMiftahulFalah <miftahridwan1101@gmail.com>
2022-12-19 14:31:06 +0700

    create category view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
   return view('index');
 });
+
+Route::resource('categories', CategoryController::class);
