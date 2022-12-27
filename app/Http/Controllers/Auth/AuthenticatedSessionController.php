463422cfe94231902ada3228baadb8597239a0df
RidwanMiftahulFalah <miftahridwan1101@gmail.com>
2022-12-28 05:25:50 +0700

    fix login bug

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller {
 
     $request->session()->regenerate();
 
-    return redirect()->route('dashboard');
+    return redirect()->route('dashboard.index');
   }
 
   /**
