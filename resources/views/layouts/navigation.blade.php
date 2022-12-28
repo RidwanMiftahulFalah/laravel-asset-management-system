0a10d9d655ee90d4d062c2461996830968b63d5d
RidwanMiftahulFalah <miftahridwan1101@gmail.com>
2022-12-28 09:03:58 +0700

    redesign navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-slate-700 border-b border-gray-100">
   <!-- Primary Navigation Menu -->
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex justify-between h-16">
@@ -6,13 +6,13 @@
         <!-- Logo -->
         <div class="shrink-0 flex items-center">
           <a href="{{ route('dashboard.index') }}">
-            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+            <x-application-logo class="block h-9 w-auto fill-current text-gray-400" />
           </a>
         </div>
 
         <!-- Navigation Links -->
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-          <x-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard')">
+        <div class="hidden sm:-my-px sm:ml-10 sm:flex">
+          <x-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard.index')">
             {{ __('Dashboard') }}
           </x-nav-link>
 
@@ -22,7 +22,7 @@
 
           @can('is-admin')
             <x-nav-link :href="route('items.index')" :active="request()->routeIs('items.*')">
-              {{ __('Aset') }}
+              {{ __('Data Aset') }}
             </x-nav-link>
 
             <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.*')">
@@ -125,8 +125,8 @@
     <!-- Responsive Settings Options -->
     <div class="pt-4 pb-1 border-t border-gray-200">
       <div class="px-4">
-        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+        <div class="font-medium text-base text-teal-500">{{ Auth::user()->name }}</div>
+        <div class="font-medium text-sm text-teal-100">{{ Auth::user()->email }}</div>
       </div>
 
       <div class="mt-3 space-y-1">
