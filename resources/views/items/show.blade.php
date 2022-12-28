bfafa1dd4c25e2d0d4995b43f713e30503efba9c
RidwanMiftahulFalah <miftahridwan1101@gmail.com>
2022-12-28 15:24:41 +0700

    redesign Item show page

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -1,78 +1,100 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+  <x-slot name="header">
+    <div class="flex justify-between">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Detail Data Aset') }}
+      </h2>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
+      <a href="{{ route('items.index') }}"
+        class="inline-flex items-center px-4 py-2 bg-teal-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+        Kembali
+      </a>
+    </div>
 
-<body>
-  <table>
-    <tr>
-      <td><label for="name">Nama Aset : </label></td>
-      <td>
-        <p id="name">{{ $item->name }}</p>
-      </td>
-    </tr>
+  </x-slot>
 
-    <tr>
-      <td><label for="is_disposable">Sifat Aset : </label></td>
-      <td>
-        <p id="is_disposable">{{ $item->is_disposable ? 'Habis Pakai' : 'Tidak Habis Pakai' }}</p>
-      </td>
-    </tr>
+  <div class="py-5">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
 
-    <tr>
-      <td><label for="stock">Stok : </label></td>
-      <td>
-        <p id="stock">{{ $item->stock }}</p>
-      </td>
-    </tr>
+          <div class="mb-3">
+            <label for="name" class="block font-medium text-sm text-gray-700">
+              Nama Aset
+            </label>
 
-    <tr>
-      <td><label for="description">Deskripsi : </label></td>
-      <td>
-        <p id="description">{{ $item->description }}</p>
-      </td>
-    </tr>
+            <input type="text" id="name" value="{{ $item->name }}" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+          </div>
 
-    <tr>
-      <td><label for="usage-permission">Hak Pakai : </label></td>
-      <td>
-        <p id="usage-permission">{{ $item->usage_permission }}</p>
-      </td>
-    </tr>
+          <div class="mb-3">
+            <label class="block font-medium text-sm text-gray-700">
+              Jenis Aset
+            </label>
 
-    <tr>
-      <td><label for="condition"></label></td>
-      <td>
-        <p id="condition">{{ $item->condition }}</p>
-      </td>
-    </tr>
+            <input type="text" id="name"
+              value="{{ $item->is_disposable ? 'Habis Pakai' : 'Tidak Habis Pakai' }}" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+          </div>
 
-    <tr>
-      <td><label for="is_active">Status : </label></td>
-      <td>
-        <p id="is_active">{{ $item->is_active }}</p>
-      </td>
-    </tr>
+          <div class="mb-3">
+            <label for="stock" class="block font-medium text-sm text-gray-700">
+              Stok
+            </label>
 
-    <tr>
-      <td><label for="category">Kategori</label></td>
-      <td>
-        <p id="category">{{ $item->category->name }}</p>
-      </td>
-    </tr>
+            <input type="number"" id="stock" value="{{ $item->stock }}" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+          </div>
 
-    <tr>
-      <td><label for="work-unit">Unit Kerja</label></td>
-      <td>
-        <p id="work-unit">{{ $item->workUnit->name }}</p>
-      </td>
-    </tr>
-  </table>
-</body>
+          <div id="condition-option" class="mb-3">
+            <label class="block font-medium text-sm text-gray-700">
+              Kondisi
+            </label>
 
-</html>
+            <input type="text" id="name" value="{{ $item->condition }}" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+          </div>
+
+          <div class="mb-3">
+            <label for="category" class="block font-medium text-sm text-gray-700">
+              Kategori
+            </label>
+
+            <input type="text" id="name" value="{{ $item->category->name }}" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+          </div>
+
+          <div class="mb-5">
+            <label for="work-unit" class="block font-medium text-sm text-gray-700">
+              Unit Kerja
+            </label>
+
+            <input type="text" id="name" value="{{ $item->workUnit->name }}" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+          </div>
+
+          <div class="mb-3">
+            <label for="hak-pakai" class="block font-medium text-sm text-gray-700">
+              Hak Pakai
+            </label>
+
+            <input type="text" id="name" value="{{ $item->usage_permission }}" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+          </div>
+
+          <div class="mb-3">
+            <label for="description" class="block font-medium text-sm text-gray-700">
+              Deskripsi
+            </label>
+
+            <textarea id="description" cols="30" rows="5" readonly
+              class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">{{ $item->description }}</textarea>
+          </div>
+
+          <script src="{{ asset('js/items/validation.js') }}"></script>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
