0ba6a974f411d9f72382f4f596e7cee1cd53751a
RidwanMiftahulFalah <miftahridwan1101@gmail.com>
2023-01-13 14:26:47 +0700

    minor design change on Categories forms

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -13,7 +13,7 @@
           @if ($errors->any())
             <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-gray-800 dark:text-red-400"
               role="alert">
-              <ul>
+              <ul class="list-disc list-outside">
                 @foreach ($errors->all() as $error)
                   <li>{{ $error }}</li>
                 @endforeach
